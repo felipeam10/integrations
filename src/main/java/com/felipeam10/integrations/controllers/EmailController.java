75d815e5231aa402188657c9d91846c8a14ea789
felipeam10 <felipeam10@hotmail.com>
2024-09-19 02:46:03 -0300

    vJSR-02.04 - Sending first email with SendGrid

--- src/main/java/com/felipeam10/integrations/controllers/EmailController.java
+++ src/main/java/com/felipeam10/integrations/controllers/EmailController.java
@@ -1,6 +1,8 @@
 package com.felipeam10.integrations.controllers;
 
 import com.felipeam10.integrations.dto.EmailDTO;
+import com.felipeam10.integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class EmailController {
 
+    @Autowired
+    private EmailService emailService;
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody  EmailDTO emailDTO) {
+        emailService.sendEmail(emailDTO);
         return ResponseEntity.noContent().build();
     }
 }
